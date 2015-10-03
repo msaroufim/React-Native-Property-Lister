3523f28ad5a47f59d7eb048cd2e14329e975e4ba
Mark Saroufim <marksaroufim@gmail.com>
2015-10-03 15:00:44 -0700

    first version of house finder UI in iOS

---
+++ index.ios.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var React      = require('react-native');
+var SearchPage = require('./SearchPage');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class HelloWorld extends React.Component {
+  render() {
+   return <React.Text style={styles.text}>Hello World (Again)</React.Text>;
+  }
+}
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+      style={styles.container}
+      initialRoute={{
+        title: 'PropertyFinder',
+        component: SearchPage,
+      }}/>
+      );
+  }
+}
+
+React.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
+
+
+
+//JSX vs vanilla js style
+// return <React.Text style={styles.text}>Hello World (Again)</React.Text>;
+//     return React.createElement(React.Text, {style: styles.text}, "Hello World!")
